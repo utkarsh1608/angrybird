f518ee7b3eb09eec8f32e9db624072c53a0416e4
utkarsh1608 <71700133+utkarsh1608@users.noreply.github.com>
2020-12-19 17:30:50 +0530

    Add files via upload

---
+++ AngryBirdsStage3.5-master/AngryBirdsStage3.5-master/Slingshot.js
@@ -0,0 +1,28 @@
+//create 
+class SlingShot{
+    constructor(bodyA, bodyB){
+        var options = {
+            bodyA: bodyA,
+            bodyB: bodyB,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.sling = Constraint.create(options);
+        World.add(world, this.sling);
+    }
+    fly(){
+     this.sling.bodyA=null
+
+
+    }
+
+    display(){
+if(this.sling.bodyA){
+        var pointA = this.chain.bodyA.position;
+        var pointB = this.chain.bodyB.position;
+        strokeWeight(4);
+        line(pointA.x, pointA.y, pointB.x, pointB.y);
+}
+    }
+    
+}
